39b2891268a055c53f413d1b2c09d44a3279bf7b
Manuel Rodrigues <manuelrodrigues0206@gmail.com>
2026-07-27 19:58:35 +0530

    Fix Vercel build

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -194,7 +194,7 @@ export default function MainLayout() {
 
         {/* Page Content - left aligned, same width */}
         <main className="flex-1 overflow-y-auto pb-20 lg:pb-0 lg:pt-16">
-          <div className={`max-w-2xl ${sidebarCollapsed ? "" : "lg:-ml-12"}`}>
+          <div className={`${sidebarCollapsed ? "max-w-2xl" : "max-w-2xl lg:max-w-xl"}`}>
             <Outlet />
           </div>
         </main>
